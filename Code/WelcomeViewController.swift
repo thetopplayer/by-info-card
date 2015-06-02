2eff9e506b075970732ce9b8a5cefc476fea04d2
Eddie Kaiger <eddiekaiger@gmail.com>
2015-06-02 09:47:00 -0700

    Created (final) welcome scene mockup. Cleaned up project.

---
+++ Code/WelcomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeViewController.swift
+//  BryteYouthInfoCard
+//
+//  Created by Eddie Kaiger on 5/31/15.
+//  Copyright (c) 2015 Eddie Kaiger. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+
+    }
+}
